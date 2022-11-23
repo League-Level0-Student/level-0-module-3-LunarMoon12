fc20e21cbeb7cd3e00078c4f7a8fc5191ccf525c
root <root@c386a0c2a095>
2022-11-23 02:22:43 +0000

    Bottles, Walk of Fame.

--- src/_02_loop_variables/_3_robot_walk_of_fame/WalkOfFame.java
+++ src/_02_loop_variables/_3_robot_walk_of_fame/WalkOfFame.java
@@ -6,21 +6,30 @@ import org.jointheleague.graphical.robot.Robot;
 public class WalkOfFame {
 	public static void main(String[] args) {
 		Robot rob = new Robot();
-		
-		// 1. Set the X position of the robot so that it starts on the left. 
+
+		// 1. Set the X position of the robot so that it starts on the left.
 		// You also need to show the robot to see the result of this line.
+		rob.show();
+		rob.setSpeed(100);
+		rob.setRandomPenColor();
+		rob.penDown();
+		for (int i = 0; i < 10; i++) {
+			rob.setX(40 + i * 50);
+			// 2. Make the robot draw a star shape. Hint: angle=144.
+			for (int n = 0; n < 5; n++) {
+				rob.move(30);
+				rob.turn(144);
+			}
+			// 3. Set the length of each line in the star to 30.
 
-		// 2. Make the robot draw a star shape. Hint: angle=144.
+			/** THE CHALLENGE: **/
+			/*
+			 * Make the robot draw a line of 10 stars (see recipe to see how it should look)
+			 * * Hint: The distance between stars is 50.
+			 */
 
-		// 3. Set the length of each line in the star to 30.
-
-		/** THE CHALLENGE: **/
-		/*
-		 * Make the robot draw a line of 10 stars (see recipe to see how it should look)
-		 * 
-		 * Hint: The distance between stars is 50.
-		 */
+		}
+		rob.hide();
 
 	}
-
 }
---
+++ src/_02_loop_variables/_4_99_bottles/bottles/bottles.pde
@@ -0,0 +1,9 @@
+public static void main (String args[]){
+for (int i = 99; i > 0; i--) {
+System.out.print(i  + "bottles of beer on the wall.");
+System.out.print(i  + "bottles of beer!" );
+System.out.print("Take one down, pass it around,");
+System.out.print(i - 1  + "bottles of beer on the wall!");
+   }
+
+}
